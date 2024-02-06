85ebd857c851fcb39defb14f3e601a375b4a3bb9
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2024-02-06 04:46:11 -0500

    created mysql database and models

---
+++ database.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS LIBRARY;
+USE LIBRARY;
+
+CREATE TABLE authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    gender VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+#One author could have many books
