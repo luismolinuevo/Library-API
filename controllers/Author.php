aa3481012590dd3c1bff1ea252768720c516c40c
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2024-02-07 21:04:38 -0500

    get all authors controller complete

--- controllers/Author.php
+++ controllers/Author.php
@@ -1,2 +1,21 @@
 <?php
 
+class BooksController {
+    private $bookModel;
+
+    public function __construct() {
+        require_once 'BookModel.php';
+        $this->bookModel = new BookModel();
+    }
+
+    public function getAllBooks() {
+        try {
+            $books = $this->bookModel->getAllBooks();
+            http_response_code(200);
+            echo json_encode(["success" => true, "data" => $books]);
+        } catch (Exception $e) {
+            http_response_code(500);
+            echo json_encode(["success" => false, "error" => "Failed to fetch books: " . $e->getMessage()]);
+        }
+    }
+}
